16437b777f39692088fa853394bd708ddd2d18a7
Ankeet Maini <ankeet.maini@gmail.com>
2020-07-09 16:20:56 +0530

    try

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use(express.static('.'))
+app.use(express.static("."));
 
-app.listen(3000, () => console.log('Express listening on port 3000...'))
+const a = app.listen(3000, () =>
+  console.log("Express listening on port 3000...")
+);
+
+process.on("SIGTERM", () => {
+  console.log("sig term...");
+  a.close((err) => {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
+
+    process.exit();
+  });
+});
+
+process.on("SIGINT", () => {
+  console.log("sig int...");
+  process.exit();
+});
