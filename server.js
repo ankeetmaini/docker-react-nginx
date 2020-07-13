b40a36349e484168df782e00099703f39dd9cec5
Ankeet Maini <ankeet.maini@gmail.com>
2020-07-13 15:33:00 +0530

    Prettify the sigterm and sigint handling

--- server.js
+++ server.js
@@ -3,23 +3,20 @@ const app = express();
 
 app.use(express.static("."));
 
-const a = app.listen(3000, () =>
+const server = app.listen(3000, () =>
   console.log("Express listening on port 3000...")
 );
 
 process.on("SIGTERM", () => {
-  console.log("sig term...");
-  a.close((err) => {
+  server.close((err) => {
     if (err) {
       console.log(err);
       process.exit(1);
     }
-
     process.exit();
   });
 });
 
 process.on("SIGINT", () => {
-  console.log("sig int...");
-  process.exit();
+  server.close(() => process.exit(0));
 });
