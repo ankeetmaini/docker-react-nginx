8bd7f6613ed75d15ee36ed805534f00c681f970f
Ankeet Maini <ankeet.maini@gmail.com>
2020-07-13 15:33:43 +0530

    Add graceful restart (#4)
    * try
    
    * Prettify the sigterm and sigint handling

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use(express.static('.'))
+app.use(express.static("."));
 
-app.listen(3000, () => console.log('Express listening on port 3000...'))
+const server = app.listen(3000, () =>
+  console.log("Express listening on port 3000...")
+);
+
+process.on("SIGTERM", () => {
+  server.close((err) => {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
+    process.exit();
+  });
+});
+
+process.on("SIGINT", () => {
+  server.close(() => process.exit(0));
+});
